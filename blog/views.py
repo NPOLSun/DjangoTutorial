155c8c0e125a456c91967e59849f061dcc1c85cf
Sunyong <sunyonge@gmail.com>
2020-06-21 20:12:54 +0900

    added app, linke each post ....

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,19 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html')
+    qs = Post.objects.all()
+    qs = qs.filter(published_date__lte=timezone.now())
+    qs = qs.order_by('published_date')
+
+    return render(request, 'blog/post_list.html', {
+        'post_list': qs,
+    })
+
+def post_detail(request,pk):
+
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post,})
